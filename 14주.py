f317ff81868cda2010891b57a16890edd6b8c96f
LEESANGNAM <fgh0116@naver.com>
2021-06-02 15:29:12 +0900

    14주

---
+++ 14주.py
@@ -0,0 +1,22 @@
+seasons=["spring","summer","fall","winter","pythong","java"]
+
+for i,s in enumerate(seasons,1):
+    print(s,i)
+
+print("===========")
+
+a=[1,2,3,5]
+b="abcd"
+c=list(zip(a,b))
+
+print(c)
+print("===========")
+
+for s,i in zip(seasons,range(0,6)):
+    print(s,i+1)
+print("===========")
+def add(a,b):
+    print("입력받은 2개의 숫자는",a,b)
+    return a+b
+c=add(1,2)
+print(c)
